68459c863a55d738fe70fab5c967c738b9481bf8
DimaVartic <119105057+DimaVartic@users.noreply.github.com>
2023-02-27 21:54:29 +0200

    Add files via upload

---
+++ HW.payment-calc.js
@@ -0,0 +1,11 @@
+/* HW2:
+        usser enters how many houyrs he works a day
+        calculate and show how much does he make each day,
+        considering he's hourly payment is : 10$ */
+
+let hourPayment = 10
+let hoursPerDay = prompt( "Insert the number of work hours per day")
+
+payment = hourPayment * hoursPerDay 
+
+alert (payment)
