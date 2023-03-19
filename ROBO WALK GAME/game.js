5cfe787e7aea8786dd474f1195337d42c768cf1b
DimaVartic <119105057+DimaVartic@users.noreply.github.com>
2023-03-19 21:49:04 +0200

    Update game.js

--- ROBO WALK GAME/game.js
+++ ROBO WALK GAME/game.js
@@ -1,25 +1,25 @@
-const STEP = 36  // = width 
-let robotLeft = 1 // coords: steps
-let robotDown = 1
+const STEP = 36  // = width
+let robotLeft = 0 // coords: steps
+let robotDown = 0
 
 
 function render() {
-    gameMap.innerHTML = 
-            `<div class="robot" 
-            
-            style="transform:translateX(${robotLeft * STEP}px)">
-            
+    gameMap.innerHTML =
+            `<div class="robot"
+
+            style="transform:translate(${robotLeft * STEP}px, ${robotDown * STEP}px)">
+
             </div>`
  }
 
- function renderY() {
-    gameMap.innerHTML = 
-            `<div class="robot" 
-            
-            style="transform:translateY(${robotDown * STEP}px)">
-            
-            </div>`
+//  function renderY() {
+//     gameMap.innerHTML =
+//             `<div class="robot"
 
- }
+//             style="transform:translateY(${robotDown * STEP}px)">
+
+//             </div>`
+
+//  }
 
  render()
