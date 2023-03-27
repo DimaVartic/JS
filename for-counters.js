f7068992a65e7c72c46ab71de71ad379856b5901
DimaVartic <119105057+DimaVartic@users.noreply.github.com>
2023-03-27 23:49:27 +0300

    Update for-counters.js

--- for-counters.js
+++ for-counters.js
@@ -12,17 +12,23 @@ for (let n=9; n>1; n--) {
 
 //HW2: refactor the gallery code using for
 
-
-
+   // let i = 9
+   // while (i > 0) {  
+     // container.innerHTML += `<img src="images/${i}.jpg">`
+      // if (i == 7 || i == 4 ) {
+        // container.innerHTML += `<br>`
+      // }
+      // i-- 
+      // }
         for  (let i = 9; i > 0;  i--) {    
             container.innerHTML += `<img src="images/${i}.jpg">`
             if (i == 7 || i == 4 ) {
                 container.innerHTML += `<br>`
             }
-
-           
         }
 
+
+        
 
 let table = `<table border "1"> <tr>`
 
